a367f57b4229baa9d7a116b2ec4bda8dbeb76255
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-10-06 19:37:25 +0530

    updation in subSection

--- controllers/SubSection.js
+++ controllers/SubSection.js
@@ -49,3 +49,52 @@ exports.subSection = async (req, res) => {
     });
   }
 };
+
+//update subSection
+exports.updateSubSection = async (req, res) => {
+  try {
+    const { subSectionId, title, timeDuration, description } = req.body;
+
+    const videoUrl = req.files.videoUrl;
+
+    if (!subSectionId || !title || !timeDuration || !description || !videoUrl) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    await SubSection.findByIdAndUpdate(
+      subSectionId,
+      { title, timeDuration, description },
+      { new: true }
+    );
+    return res.status(201).json({
+      success: true,
+      message: "SUB Section updated sucessfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//DELETE SUBSECTION
+exports.deleteSubSection = async (req, res) => {
+  try {
+    const { subSectionId } = req.body;
+    await SubSection.findByIdAndDelete(subSectionId);
+
+    return res.status(202).json({
+      success: true,
+      message: "Sub Section deleted sucessfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
